108d8b4f7ce4168932828a90f6fa3418f062b005
Oliver <local@computer.com>
2026-01-22 20:16:54 +0100

    Signed-off-by: Oliver <local@computer.com>

--- src/iPhoto/gui/ui/controllers/playback_state_manager.py
+++ src/iPhoto/gui/ui/controllers/playback_state_manager.py
@@ -11,7 +11,6 @@ from PySide6.QtCore import QObject, Signal
 from ....config import VIDEO_COMPLETE_HOLD_BACKSTEP_MS
 from ..media import MediaController, PlaylistController
 from ..models.asset_model import AssetModel, Roles
-from .detail_ui_controller import DetailUIController
 from .dialog_controller import DialogController
 
 
@@ -38,7 +37,6 @@ class PlaybackStateManager(QObject):
         media: MediaController,
         playlist: PlaylistController,
         model: AssetModel,
-        detail_ui: DetailUIController,
         dialog: DialogController,
         parent: Optional[QObject] = None,
     ) -> None:
@@ -48,7 +46,6 @@ class PlaybackStateManager(QObject):
         self._media = media
         self._playlist = playlist
         self._model = model
-        self._detail_ui = detail_ui
         self._dialog = dialog
         self._state = PlayerState.IDLE
         self._pending_live_photo_still: Optional[Path] = None
